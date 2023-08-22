016835c07aa5a818f0e4b1def15ac89ea744b98e
Juri Sajzew <j.sajzew@outlook.de>
2023-08-22 18:06:19 +0200

    detail map created
    board.js extended by the detailCard
    created various representation

--- js/board.js
+++ js/board.js
@@ -1,12 +1,44 @@
-let toDos = [];
-let progress = [];
-let feedbacks = [];
-let done = [];
+let allTasks = [
+    {
+        "task": 'User Story',
+        "designation": 'Kochwelt Page & Recipe Recomender',
+        "description": 'Build start page wirh recipe recommendation',
+        "date": '10.05.2023',
+        "priority": 'Medium',
+        "assigned": ["Emmanuel Mauerer", "Marcel Bauer", "Anton Mayeer"]
+    },
+    {
+        "task": 'Technical Task',
+        "designation": 'CSS Architecture Planning',
+        "description": 'Define CSS naming conventions and structure.',
+        "date": '02.09.2023',
+        "priority": 'Urgent',
+        "assigned": ['Juri Sajzew', 'Benedigt Ziegler']
+    }
+];
 
 
 function init() {
-
+    //includeHTML();
+    detailCard();
 }
+
+//async function includeHTML() {
+//    let includeElements = document.querySelectorAll('[w3-include-html]');
+//    let file;
+//
+//    for (let i = 0; i < includeElements.length; i++) {
+//        const element = includeElements[i];
+//        file = element.getAttribute('w3-include-html');
+//        let resp = await fetch(file);
+//
+//        if (resp.ok) {
+//            element.innerHTML = await resp.text();
+//        } else {
+//            element.innerHTML = 'Page not found.';
+//        };
+//    }
+//}
 
 
 
@@ -63,4 +95,72 @@ function openAddTask() {
 function einBlenden() {
     document.getElementById('noTask').style.display = 'none';
     document.getElementById('noTaskContain').style.display = '';
+}
+
+function detailCard() {
+    let detail = document.getElementById('detailCard');
+    detail.innerHTML = '';
+
+    for (let i = 0; i < allTasks.length; i++) {
+        const task = allTasks[i];
+
+        document.getElementById('detailCard').innerHTML += /*html*/`
+            <div class="taskOverlay">
+                <div class="frame203">
+                    <span class="frame113">${task['task']}</span>
+                    <div class="close1">
+                        <img class="close2" src="assets/img/close.svg" alt="">
+                    </div>
+                </div>
+                <p class="taskOverlayHeadline">${task['designation']}</p>
+                <div class="frame179">
+                    <p class="taskOverlayDate">Due date:</p>
+                    <span class="taskOverlayNumber">${task['date']}</span>
+                </div>
+                <div class="frame178">
+                    <p class="priority">Priority:</p>
+                    <div class="priorityButton"></div>
+                </div>
+                <div class="frame214">
+                    <p class="assignedTo">Assigned To:</p>
+                    <div class="frame204">
+                        <div class="contactContain">
+                            <div class="frame191">
+                                <div class="profileBadge">
+                                    <div class="group9">
+                                        <p class="initialien"></p>
+                                        <div class="ellipse5"></div>
+                                    </div>
+                                </div>
+                                <p class="userName6"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="frame215">
+                   <p class="subtasks">Subtasks</p>
+                   <div class="frame204">
+                        <span class="subtasksCheck">
+                            <p class="contactForm"></p>
+                        </span>
+                   </div>
+                </div>
+                <div class="frame20">
+                    <div class="deleteContact">
+                        <div class="deleteContain">
+                            <img src="assets/img/delete.svg" alt="" class="deleteIcon">
+                        </div>
+                        <p class="deleteText">Delete</p>
+                    </div>
+                    <span class="vector3"></span>
+                    <div class="editContacts">
+                        <div class="editContain">
+                            <img src="assets/img/edit.svg" alt="" class="editIcon">
+                        </div>
+                        <p class="editText">Edit</p>
+                    </div>
+                </div>
+            </div>
+    `;
+    }
 }
