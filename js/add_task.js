88927921f37bbdf602e7d4c1f8a6d75c9de612ee
Markus Gede <markusgede93@gmail.com>
2023-08-22 14:19:30 +0200

    design with bootstrap

--- js/add_task.js
+++ js/add_task.js
@@ -1,33 +1,57 @@
-function colorRed(){
+function colorRed() {
     let btn = document.getElementById('btn_urgent');
-    if(btn.classList.length>1){
-        btn.classList.remove('button-red');
+    if (btn.classList.length > 1) {
+        whiteBackgroundRedArrow(btn);
     }
-    else{
+    else {
         btn.classList.add('button-red');
+        document.getElementById('arrowWhiteUrgent').classList.add('z-index-1');
+        document.getElementById('arrowRedUrgent').classList.add('z-index-n1');
     }
-    document.getElementById('btn_medium').classList.remove('button-yellow');
-    document.getElementById('btn_low').classList.remove('button-green');
+    whiteBackgroundOrangeArrow(document.getElementById('btn_medium'));
+    whiteBackgroundGreenArrow(document.getElementById('btn_low'));
 }
-function colorYellow(){
+function colorOrange() {
     let btn = document.getElementById('btn_medium');
-    if(btn.classList.length>1){
-        btn.classList.remove('button-yellow');
+    if (btn.classList.length > 1) {
+        whiteBackgroundOrangeArrow(btn);
     }
-    else{
-        btn.classList.add('button-yellow');
+    else {
+        btn.classList.add('button-orange');
+        document.getElementById('arrowWhiteMedium').classList.add('z-index-1');
+        document.getElementById('arrowOrangeMedium').classList.add('z-index-n1');
     }
-    document.getElementById('btn_urgent').classList.remove('button-red');
-    document.getElementById('btn_low').classList.remove('button-green');
+    whiteBackgroundRedArrow(document.getElementById('btn_urgent'));
+    whiteBackgroundGreenArrow(document.getElementById('btn_low'));
 }
-function colorGreen(){
+function colorGreen() {
     let btn = document.getElementById('btn_low');
-    if(btn.classList.length>1){
-        btn.classList.remove('button-green');
+    if (btn.classList.length > 1) {
+        whiteBackgroundGreenArrow(btn);
     }
-    else{
+    else {
         btn.classList.add('button-green');
+        document.getElementById('arrowWhiteLow').classList.add('z-index-1');
+        document.getElementById('arrowGreenLow').classList.add('z-index-n1');
     }
-    document.getElementById('btn_urgent').classList.remove('button-red');
-    document.getElementById('btn_medium').classList.remove('button-yellow');
+    whiteBackgroundRedArrow(document.getElementById('btn_urgent'));
+    whiteBackgroundOrangeArrow(document.getElementById('btn_medium'));
+}
+
+function whiteBackgroundRedArrow(btn) {
+    btn.classList.remove('button-red');
+    document.getElementById('arrowWhiteUrgent').classList.remove('z-index-1');
+    document.getElementById('arrowRedUrgent').classList.remove('z-index-n1');
+}
+
+function whiteBackgroundOrangeArrow(btn) {
+    btn.classList.remove('button-orange');
+    document.getElementById('arrowWhiteMedium').classList.remove('z-index-1');
+    document.getElementById('arrowOrangeMedium').classList.remove('z-index-n1');
+}
+
+function whiteBackgroundGreenArrow(btn) {
+    btn.classList.remove('button-green');
+    document.getElementById('arrowWhiteLow').classList.remove('z-index-1');
+    document.getElementById('arrowGreenLow').classList.remove('z-index-n1');
 }
