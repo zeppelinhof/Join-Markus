47d479a6b2fc7b83cde7aa6b97be7231bc8634ba
Markus Gede <markusgede93@gmail.com>
2023-08-24 11:10:09 +0200

    Assigned to circle

--- js/load_contacts_to_add_task.js
+++ js/load_contacts_to_add_task.js
@@ -14,10 +14,12 @@ async function loadUsers() {
 async function load_contacts() {
     registerBtn.disabled = true;
     users.push({
+        name: _name.value,
         email: email.value,
         password: password.value,
+        phone: phone.value
     });
-
+    // Save local array 'users' in global backend array 'users' via post
     await setItem('users', JSON.stringify(users));
 
     fillAssignedTo();
@@ -25,52 +27,81 @@ async function load_contacts() {
 }
 
 function resetForm() {
+    _name.value = '';
     email.value = '';
     password.value = '';
+    phone.value = '';
+    users = [];
     registerBtn.disabled = false;
+    location.reload();
 }
 
 function fillAssignedTo() {
-    for (let i = 0; i < users.length; i++) {    
-        let user = users[i].email;
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i].name;
         document.getElementById('selectAssignedTo').innerHTML += /*html*/`
-        <li onclick="contactToTask(${i}, ${user})" class="list-group-item">            
+        <li class="list-group-item">            
             <div class="list-item-display-flex">
-                <div class="list-item-display-flex-icon-name">
+                <div class="list-item-display-flex-icon-name" onclick="contactToTaskClickName(${i}, '${user}')">
                     <div id="contactCirle${i}" class="contactCirle"></div>
                     <div id="option${i}"></div>
                 </div>
-                <input class="form-check-input me-1 checkbox-padding" type="checkbox" value="" id="flexCheckDefault${i}">
+                <input class="form-check-input me-1 checkbox-padding" type="checkbox" onclick="contactToTaskClickCheckbox(${i}, '${user}')" id="flexCheckDefault${i}" value="">
             </div>
         </li>                  
-    `        
+    `
         document.getElementById(`option${i}`).innerHTML += user;
-        document.getElementById(`contactCirle${i}`).innerHTML += user.charAt(0);
-        document.getElementById(`contactCirle${i}`).innerHTML += user.charAt(1);
+        initialsInCirle(i, user);
+    }
+}
+// Create Circles in list
+function initialsInCirle(i, user) {
+    document.getElementById(`contactCirle${i}`).innerHTML += user.charAt(0);
+    if (user.match(' ')) {
+        document.getElementById(`contactCirle${i}`).innerHTML += user.charAt(user.match(' ').index + 1);
     }
 }
 
 // #endregion Load Users from Backend
 
 // #region Load Users to Dropdown
-function contactToTask(i, contact){
+function contactToTaskClickName(i, user) {
     let checkbox = document.getElementById(`flexCheckDefault${i}`);
     if (checkbox.checked == true) {
         document.getElementById(`flexCheckDefault${i}`).checked = false;
-        deleteContact(contact);
+        deleteContact(user);
     }
-    else{
+    else {
+        // Create Circles of selected users below list
         contactsInTask.push(document.getElementById(`option${i}`).innerHTML);
+        document.getElementById('selected-contacts-circles-below').innerHTML += /*html*/`
+            <div class="contactCirle" id ="contactCircleBelow${i}"></div>       
+        `
+        document.getElementById(`contactCircleBelow${i}`).innerHTML += user.charAt(0);
+        if (user.match(' ')) {
+            document.getElementById(`contactCircleBelow${i}`).innerHTML += user.charAt(user.match(' ').index + 1);
+        }
+        
         document.getElementById(`flexCheckDefault${i}`).checked = true;
-    }   
+    }
 }
 
-function deleteContact(contact){
+function contactToTaskClickCheckbox(i, contact) {
+    let checkbox = document.getElementById(`flexCheckDefault${i}`);
+    if (checkbox.checked == true) {
+        contactsInTask.push(document.getElementById(`option${i}`).innerHTML);
+    }
+    else {
+        deleteContact(contact);
+    }
+}
+
+function deleteContact(contact) {
     for (let i = 0; i < contactsInTask.length; i++) {
         const currentContact = contactsInTask[i];
 
         if (contact === currentContact) {
-            contactsInTask.splice(i,1);
+            contactsInTask.splice(i, 1);
         }
     }
 }
