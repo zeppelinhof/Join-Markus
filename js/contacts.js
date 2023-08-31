54828d3f59c67a4669cd373fb586d3a178c468f5
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2023-08-31 18:34:39 +0200

    add contact colors to global.js

--- js/contacts.js
+++ js/contacts.js
@@ -47,85 +47,6 @@ let contacts = [
 ];
 
 
-/* 
-* colors from components
-* form left to right
-* and
-* top to buttom
-*/
-let contactColors = [
-    {
-        'number': 1,
-        'name': 'orange_1',
-        'style': 'rgb(255,122,0)'
-    },
-    {
-        'number': 2,
-        'name': 'pink_1',
-        'style': 'rgb(255,94,179)'
-    },
-    {
-        'number': 3,
-        'name': 'blue_purple',
-        'style': 'rgb(110,81,255)'
-    },
-    {
-        'number': 4,
-        'name': 'purple',
-        'style': 'rgb(147,39,255)'
-    },
-    {
-        'number': 5,
-        'name': 'turquoise',
-        'style': 'rgb(1,190,232)'
-    },
-    {
-        'number': 6,
-        'name': 'seagreen',
-        'style': 'rgb(31,215,193)'
-    },
-    {
-        'number': 7,
-        'name': 'orange_red',
-        'style': 'rgb(255,116,94)'
-    },
-    {
-        'number': 8,
-        'name': 'orange_2',
-        'style': 'rgb(255,163,94)'
-    },
-    {
-        'number': 9,
-        'name': 'pink_2',
-        'style': 'rgb(252,113,255)'
-    },
-    {
-        'number': 10,
-        'name': 'yellow_1',
-        'style': 'rgb(252,199,1)'
-    },
-    {
-        'number': 11,
-        'name': 'blue',
-        'style': 'rgb(0,56,255)'
-    },
-    {
-        'number': 12,
-        'name': 'yellow_2',
-        'style': 'rgb(255,230,43)'
-    },
-    {
-        'number': 13,
-        'name': 'red',
-        'style': 'rgb(255,70,70)'
-    },
-    {
-        'number': 14,
-        'name': 'ocher',
-        'style': 'rgb(255,187,43)'
-    }
-]
-
 let existLetterHeadline = '';
 
 
@@ -232,11 +153,4 @@ function closeAllContactClicks() {
 
         currentClick.classList.remove('contact-card-click');
     }
-}
-
-
-function returnContactColor(i) {
-    let result = i % contactColors.length
-
-    return contactColors[result]['style'];
 }
